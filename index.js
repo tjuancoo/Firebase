5ae11765a932f34a263c4663c1671da3740568b6
Juan Carlos <jctapia222@gmail.com>
2023-02-09 20:23:14 -0600

    Agregué el CRUD

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js";
+import { collection, addDoc, doc, setDoc, deleteDoc, getDocs, updateDoc } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js";
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -13,6 +13,7 @@ import {
   signInWithPopup,
   getRedirectResult,
 } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyA8OTw0wrGKWvdX-5SXKBBMs5CGcjXpelE",
@@ -27,6 +28,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+
 
 let crear = document.getElementById("btnCrear");
 let email = document.getElementById("inEmail");
@@ -122,6 +124,7 @@ google.addEventListener("click", function () {
       const user = result.user;
       document.getElementById('form').style.display = 'block';
       document.getElementById('reg').style.display = 'none';
+      
       usuario.innerHTML = `<p class="user">Bienvenido ${user.email} </p>`
       // ...
     }).catch((error) => {
@@ -141,53 +144,95 @@ const guardar = document.getElementById("btnGuardar");
 const nombre = document.getElementById("nom");
 const apPat = document.getElementById("app");
 const apMat = document.getElementById("apm");
+let idU = document.getElementById("idU")
 guardar.addEventListener("click", async () => {
-  
-  try {
-    const docRef = await addDoc(collection(db, "users"),  {
-      Nombre: `${nombre.value}`,
-      ApellidoPat: `${apPat.value}`,
-      ApellidoMat: `${apMat.value}`
+  try{
+    await setDoc(doc(db, "users", idU.value), {
+      id: idU.value,
+      name: nombre.value,
+      app: apPat.value,
+      apm: apMat.value
     });
     alert("Se han guardado sus datos correctamente");
-    document.getElementById('form').style.display = 'none';
-    document.getElementById('reg').style.display = 'block';
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-    alert("Error al guardar los datos")
+
   }
+  catch(error) {
+    alert(error)
+  }
+
 });
+
+
+const actualizar = document.getElementById("btnVer")
+const tabla = document.getElementById("table")
+actualizar.addEventListener("click", async () => {
+  tabla.innerHTML =
+      `<tr>
+      <td>Id</td>
+      <td>Nombre</td>
+      <td>Apellido Paterno</td>
+      <td>Apellido Materno</td>
+  </tr>`;
+  document.getElementById('table').style.display = 'block';
+
+  const querySnapshot = await getDocs(collection(db, "users"));
+  querySnapshot.forEach((doc) => {
+
+      console.log(doc.id, " => ", doc.data());
+      tabla.innerHTML +=
+          `<tr>
+          <td>${doc.id}</td>
+          <td>${doc.data().name}</td>
+          <td>${doc.data().app}</td>
+          <td>${doc.data().apm}</td>
+      </tr>`;
+  });
+});
+
+const update = document.getElementById("btnUpdate")
+update.addEventListener("click", async() => {
+  const elementRef = doc(db, "users", idU.value);
+
+  await updateDoc(elementRef, {
+      name: nombre.value,
+      app: apPat.value,
+      apm: apMat.value
+  });
+});
+
+const eliminar = document.getElementById("btnEliminar")
+eliminar.addEventListener("click", async()=>{
+  await deleteDoc(doc(db, "users", idU.value));
+})
 
 const providerFacebook = new FacebookAuthProvider();
 let facebook = document.getElementById("btnFace");
-facebook.addEventListener("click", function(){
+facebook.addEventListener("click", function () {
   signInWithPopup(auth, providerFacebook)
-  .then((result) => {
-    // The signed-in user info.
-    const user = result.user;
+    .then((result) => {
+      // The signed-in user info.
+      const user = result.user;
 
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    const credential = FacebookAuthProvider.credentialFromResult(result);
-    const accessToken = credential.accessToken;
-    document.getElementById('form').style.display = 'block';
-    document.getElementById('reg').style.display = 'none';
-    usuario.innerHTML = `<p class="user">Bienvenido ${user.email} </p>`
-    // ...
-  })
-  .catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = FacebookAuthProvider.credentialFromError(error);
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      const credential = FacebookAuthProvider.credentialFromResult(result);
+      const accessToken = credential.accessToken;
+      document.getElementById('form').style.display = 'block';
+      document.getElementById('reg').style.display = 'none';
+      usuario.innerHTML = `<p class="user">Bienvenido ${user.email} </p>`
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = FacebookAuthProvider.credentialFromError(error);
 
-    // ...
-  });
+      // ...
+    });
 })
-
 
 
 
